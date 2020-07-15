746c453a3298b23440931a2342442eeeb8de04a1
Harry Kwon <harrykwon20@gmail.com>
2020-07-15 15:09:48 +0900

    Added basic player controls demo

---
+++ BobTheBlob/Assets/Scripts/PlayerControls/IMovementStrategy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public interface IMovementController{
+    void Initialize();
+}
+
+public class MobilityMovementController : MonoBehaviour, IMovementController
+{
+    Rigidbody2D rb;
+    PlayerController player;
+    public void Initialize()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<PlayerController>();
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateHorizontalMovement();
+        UpdateJumpMovement();
+        UpdateLauchMovement();
+    }
+
+    void UpdateHorizontalMovement()
+    {
+        // horizontal acceleration
+        float inputX = Input.GetAxis("Horizontal");
+        if(rb.velocity.magnitude < player.SPEED_MAX)
+        {
+            rb.AddForce(Vector2.right * inputX * player.ACCELERATION_RATE);
+        }
+    }
+
+    void UpdateJumpMovement()
+    {
+        // jump mechanics
+        if(player.isGrounded)
+        {
+            float inputJump = Input.GetAxis("Jump");
+            rb.velocity = rb.velocity + (Vector2) transform.up * inputJump * player.JUMP_SPEED;
+        }
+    }
+
+    bool dragging = false;
+    Vector3 dragStartPosition;
+    void UpdateLauchMovement()
+    {
+        // drag started
+        if (Input.GetMouseButton(1) && !dragging)
+        {
+            dragStartPosition = Input.mousePosition;
+            dragging = true;
+        }
+
+        // drag ended
+        if(!Input.GetMouseButton(1) && dragging)
+        {
+            Launch((Vector2) (dragStartPosition - Input.mousePosition));
+            dragging = false;
+        }
+    }
+
+    void Launch(Vector2 direction)
+    {
+        rb.velocity = rb.velocity + direction.normalized * player.ACCELERATION_RATE;
+    }
+
+
+
+}
---
+++ BobTheBlob/Assets/Scripts/PlayerControls/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float ACCELERATION_RATE;
+    public float SPEED_MAX;
+    public float GROUNDED_EPSILON = 0.01f;
+    public float JUMP_SPEED;
+
+    public enum PlayerState {
+        Speed
+    }
+    PlayerState currentState;
+    IMovementController iMovementController;
+
+    void ChangeState(PlayerState nextState)
+    {
+        // remove the current movement controller
+        if(iMovementController != null)
+        {
+            Destroy((Component) iMovementController);
+        }
+
+        switch (nextState) {
+            case PlayerState.Speed:
+                iMovementController = gameObject.AddComponent<MobilityMovementController>();
+                break;
+        }
+
+        iMovementController.Initialize();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ChangeState(PlayerState.Speed);
+    }
+
+    private void Update()
+    {
+        // set sprite x direction
+        if(rb.velocity.x != 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = rb.velocity.x < 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGroundedState();
+    }
+
+    // grounded state update
+    public bool isGrounded { get; private set; }
+
+    void CheckGroundedState()
+    {
+        float hitboxHeight = 0f;//transform.lossyScale.y * 0.5f; //placeholder until shape of hitbox is decided on, can do vertex comparison for variable hitbox mesh
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down);
+        Debug.DrawRay(transform.position, Vector2.down);
+
+        isGrounded = false;
+        foreach(RaycastHit2D hit in hits)
+        {
+            if(hit.collider.gameObject == this.gameObject)
+            {
+                continue;
+            }
+            if(hit.distance <= GROUNDED_EPSILON + hitboxHeight)
+            {
+                isGrounded = true;
+            }
+        }
+
+
+    }
+
+}
